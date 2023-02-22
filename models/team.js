96e337fde553c738176d97e0cce74bec22e92fe1
Taner <taneozer16@gmail.com>
2023-02-23 01:36:24 +0300

    team modeli eklendi

---
+++ models/team.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+
+const teamSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  location: {
+    type: String,
+    required: true
+  },
+  stadium: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Stadium',
+    required: true
+  },
+  coach: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Coach',
+    required: true
+  },
+  founded: {
+    type: Number,
+    required: true
+  },
+  championships: {
+    type: Number,
+    required: true
+  },
+  logoUrl: {
+    type: String,
+    required: true
+  },
+  players: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Player'
+  }]
+});
+
+teamSchema.statics.findByName = function(name, callback) {
+  return this.findOne({ name }, callback);
+};
+
+const Team = mongoose.model('Team', teamSchema);
+
+module.exports = Team;
