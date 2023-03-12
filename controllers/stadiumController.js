1fde3a58108d44318d4621776a486a821eeec08c
Taner <taneozer16@gmail.com>
2023-03-12 14:42:21 +0300

    stadiumController edited

--- controllers/stadiumController.js
+++ controllers/stadiumController.js
@@ -6,14 +6,14 @@ exports.createStadium = async (req, res) => {
     const { teamId } = req.body;
     const existingStadium = await Stadium.findOne({ name: req.body.name });
     if (existingStadium) {
-      return res.status(400).json({ message: 'Stadium already exists' });
+      return res.status(400).json({ message: 'Stadium already exists', success: false });
     }
 
     let team = null;
     if (teamId) {
       team = await Team.findById(teamId);
       if (!team) {
-        return res.status(400).json({ message: 'Invalid team ID' });
+        return res.status(400).json({ message: 'Invalid team ID', success: false });
       }
     }
 
@@ -30,52 +30,58 @@ exports.createStadium = async (req, res) => {
     });
 
     const savedStadium = await stadium.save();
-    res.status(201).json(savedStadium);
+    res.status(201).json({ message: 'Stadium Added', success: true, data: savedStadium });
   } catch (err) {
     console.error(err);
-    res.status(500).json({ message: 'Server error' });
+    res.status(500).json({ message: 'Server error', success: false });
   }
 };
 
 exports.getStadiums = async (req, res) => {
   try {
     const stadiums = await Stadium.find().populate('teams');
-    res.status(200).json({ success: true, data: stadiums });
+    res.status(200).json({ success: true, data: stadiums, message: "Stadiums retrieved successfully." });
   } catch (error) {
-    res.status(500).json({ success: false, error: error.message });
+    console.error(error);
+    res.status(500).json({ success: false, error: "An error occurred while retrieving stadiums." });
   }
 };
+
 
 exports.getStadiumById = async (req, res) => {
   try {
     const stadium = await Stadium.findById(req.params.id).populate('teams');
     if (!stadium) {
-      return res.status(404).json({ message: 'Stadium not found' });
+      return res.status(404).json({ success: false, message: "Stadium not found" });
     }
-    res.json(stadium);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: 'Server error' });
+    res.status(200).json({ success: true, data: stadium, message: "Stadium retrieved successfully" });
+  } catch (error) {
+    res.status(500).json({ success: false, error: error.message });
   }
 };
 
-exports.updateStadium = async (req, res) => {
-  const id = req.params.id;
-  const stadiumData = req.body;
 
-  Stadium.findOneAndUpdate(
-    { _id: id },
-    stadiumData,
-    { new: true }
-  )
-    .then(stadium => {
-      if (!stadium) {
-        return res.status(404).json({ error: 'stadium not found' });
-      }
-      res.status(200).json({ stadium: stadium });
-    })
-    .catch(err => next(err));
+exports.updateStadium = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const stadiumData = req.body;
+
+    const stadium = await Stadium.findOneAndUpdate(
+      { _id: id },
+      stadiumData,
+      { new: true }
+    );
+
+    if (!stadium) {
+      return res.status(404).json({ success: false, message: 'Stadium not found' });
+    }
+
+    res.status(200).json({ success: true, stadium: stadium, message: 'Stadium updated successfully' });
+  } catch (error) {
+    res.status(500).json({ success: false, error: error.message });
+  }
 };
+
 
 exports.deleteStadium = async (req, res, next) => {
   const { id } = req.params;
@@ -83,34 +89,34 @@ exports.deleteStadium = async (req, res, next) => {
   try {
     const stadiumData = await Stadium.findByIdAndDelete(id);
     if (!stadiumData) {
-      return res.status(404).json({ message: 'Stadium not found' });
+      return res.status(404).json({ message: 'Stadium not found', success: false });
     }
-    res.status(200).json({ message: 'Stadium deleted successfully' });
+    res.status(200).json({ message: 'Stadium deleted successfully', success: true, data: stadiumData });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 };
 
-exports.addTeamToStadium =  async (req, res) => {
+exports.addTeamToStadium = async (req, res) => {
   try {
-      const { stadiumId } = req.params;
-      const { teamName } = req.body;
-      const stadium = await Stadium.findById(req.params.stadiumId);
-      if (!stadium) {
-          return res.status(404).json({ message: 'stadium not found' });
-      }
-      const existingTeam = await Team.findOne({ name: teamName });
-      if (!existingTeam) {
-          return res.status(404).json({ message: 'Team not found' });
-      }
-      if (existingTeam.stadium) {
-          return res.status(400).json({ message: 'Team already has a stadium' });
-      }
-      existingTeam.stadium = stadium;
-      await existingTeam.save();
-      res.json({ message: 'stadium added to team successfully' });
+    const { stadiumId } = req.params;
+    const { teamName } = req.body;
+    const stadium = await Stadium.findById(req.params.stadiumId);
+    if (!stadium) {
+      return res.status(404).json({ message: 'stadium not found', success: false });
+    }
+    const existingTeam = await Team.findOne({ name: teamName });
+    if (!existingTeam) {
+      return res.status(404).json({ message: 'Team not found', success: false });
+    }
+    if (existingTeam.stadium) {
+      return res.status(400).json({ message: 'Team already has a stadium', success: false });
+    }
+    existingTeam.stadium = stadium;
+    await existingTeam.save();
+    res.json({ message: 'stadium added to team successfully', success: true, data: stadium });
   } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: 'Server Error' });
+    console.error(error);
+    res.status(500).json({ message: 'Server Error', success: false });
   }
 };
